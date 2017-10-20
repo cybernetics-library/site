e6e16bdbb7f7f5a939bdc30ea58e1878560fba21
melaniehoff <m@melanie-hoff.com>
2017-10-20 14:50:23 -0400

    scroll-animate

--- main.js
+++ main.js
@@ -1,1 +1,68 @@
 
+
+$(document).ready(function() {
+
+
+  var scrollTop = $(".scrollTop");
+
+  $(window).scroll(function() {
+
+    var topPos = $(this).scrollTop();
+
+    // if user scrolls down - show scroll to top button
+    if (topPos > 100) {
+      $(scrollTop).css("opacity", "1");
+
+    } else {
+      $(scrollTop).css("opacity", "0");
+    }
+
+  });
+
+  //Click event to scroll to top
+  $(scrollTop).click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+
+  });
+
+
+
+  var w = $("#workshop").position();
+  var l = $("#lib").position();
+  var o = $("#organizers").position();
+  var s = $("#sponsors").position();
+
+  $('#rn-w').click(function() {
+    $('html, body').animate({
+      scrollTop: w.top
+    }, 500);
+    return false;
+
+  });
+
+  $('#rn-l').click(function() {
+    $('html, body').animate({
+      scrollTop: l.top
+    }, 500);
+    return false;
+
+  });
+
+  $('#rn-o').click(function() {
+    $('html, body').animate({
+      scrollTop: o.top
+    }, 500);
+    return false;
+  });
+
+  $('#rn-s').click(function() {
+    $('html, body').animate({
+      scrollTop: s.top
+    }, 500);
+    return false;
+  });
+
+}); // ready() END
